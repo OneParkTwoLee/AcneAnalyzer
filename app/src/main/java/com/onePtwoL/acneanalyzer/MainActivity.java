1679fe97db58a57cdfc4b6aeedead60ee253647a
jun991203 <jun991203@naver.com>
2020-08-02 01:20:24 +0900

    first Commit

---
+++ app/src/main/java/com/onePtwoL/acneanalyzer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.onePtwoL.acneanalyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
