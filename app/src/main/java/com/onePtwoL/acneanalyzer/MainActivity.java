d0eb723a82f849dc8708c6318b7bd969170f3743
jun991203 <jun991203@naver.com>
2020-08-05 01:43:44 +0900

    crop image from gallery, success!

--- app/src/main/java/com/onePtwoL/acneanalyzer/MainActivity.java
+++ app/src/main/java/com/onePtwoL/acneanalyzer/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -61,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int CROP_PICTURE = 3;
     private static final int PICK_FROM_FILE = 4;
 
-    private static Uri imageUri;
+    private static Uri imageUri, albumUri;
     private String imageFilePath, imageFileName;
     private Bitmap imageBitmap;
 
@@ -175,34 +176,40 @@ public class MainActivity extends AppCompatActivity {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = null;
-            photoFile = createImageFile();
+            try{
+                photoFile = createImageFile();
+                Log.d("카메라 찍음", photoFile.toString()+"");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "com.onePtwoL.acneanalyzer.fileprovider", photoFile);
+                imageUri = photoURI;
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
+
+                Log.d("넘어감?", "OK");
             }
         }
     }
 
     /* 이미지 파일 생성하는 함수 */
-    private File createImageFile(){
+    private File createImageFile() throws IOException{
         // 이미지 파일 이름
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        imageFileName =  "test_" +timeStamp ;
+        imageFileName =  "test_" +timeStamp+".jpg" ;
         Log.d("이미지 파일 타임스탬프", imageFileName+"");
 
         // 이미지가 저장될 폴더 이름(outline) & 빈 폴더 생성
-        File storageDir = Environment.getExternalStorageDirectory(); // 외부 저장소 (공용공간) -> /storage/emulated/0/
+        //File storageDir = Environment.getExternalStorageDirectory(); // 외부 저장소 (공용공간) -> /storage/emulated/0/
+        File storageDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
         if(storageDir.exists()){
             storageDir.mkdirs();
         }
         File image = null;
-        try {
-            image = File.createTempFile(imageFileName, ".jpg", storageDir);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        image = new File(storageDir, imageFileName);
         imageFilePath = image.getAbsolutePath();
         Log.d("이미지 파일 절대경로", imageFilePath+"");
         return image;
@@ -210,9 +217,10 @@ public class MainActivity extends AppCompatActivity {
 
     /* 갤러리에 저장하는 함수 */
     private void saveImage(Bitmap finalBitmap){
-        String imageFileForGalName =  imageFileName + ".jpg";
+        String imageFileForGalName =  imageFileName;
         Log.d("갤러리 저장될 때", imageFileForGalName+"");
-        File myDir = new File(Environment.getExternalStorageDirectory().toString());
+        //File myDir = new File(Environment.getExternalStorageDirectory().toString());
+        File myDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
         myDir.mkdirs();
         File file = new File(myDir, imageFileForGalName);
         try{
@@ -228,9 +236,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void galleryAddPic(){
+        Log.d("갤러리애드픽", "OK");
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File file = new File(imageFilePath);
+        Uri uri = Uri.fromFile(file);
+        mediaScanIntent.setData(uri);
+        sendBroadcast(mediaScanIntent);
+    }
+
     /* 갤러리 intent 연결하는 함수 */
     private void selectFromGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, PICK_FROM_ALBUM);
     }
@@ -244,6 +262,20 @@ public class MainActivity extends AppCompatActivity {
         return cursor.getString(column_index);
     }
 
+    /* 이미지 크롭 */
+    public void cropImage(){
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(imageUri, "image/*");
+
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.putExtra("output", albumUri);
+        startActivityForResult(intent, CROP_PICTURE);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -254,8 +286,10 @@ public class MainActivity extends AppCompatActivity {
                 case PICK_FROM_CAMERA: {
                     if (resultCode == RESULT_OK) {
                         File file = new File(imageFilePath);
+                        Log.d("카메라 파일", imageFilePath+"");
                         BitmapFactory.Options options = new BitmapFactory.Options();
                         imageBitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                        Log.d("비트맵 변환 성공", "OK");
 
                         if (imageBitmap != null) {
                             ExifInterface ei = new ExifInterface(imageFilePath);
@@ -263,13 +297,12 @@ public class MainActivity extends AppCompatActivity {
                             int exifDegree = exifOrientationToDegrees(orientation);
 
                             Bitmap rotatedBitmap = rotateImage(imageBitmap, exifDegree);
-
                             setRecyclerViewData(imageFilePath);  // RecyclerView에 데이터 추가
                             Log.d("카메라 이미지 URI_re", imageUri+"");
 
                             saveImage(rotatedBitmap);   // 갤러리에 저장하는 함수
-                            //startActivityForResult(intent, CROP_FROM_CAMERA);
                         }
+
                     }
                     break;
 
@@ -277,26 +310,35 @@ public class MainActivity extends AppCompatActivity {
                 // 갤러리에서 이미지 불러오는 경우
                 case PICK_FROM_ALBUM :{
                     if(resultCode == RESULT_OK){
-                        Uri ImageUri = data.getData();
-                        imageFilePath = getImagePathFromURI(ImageUri);
+                        imageUri = data.getData();
+                        imageFilePath = getImagePathFromURI(imageUri);
                         Log.d("갤러리이미지path", imageFilePath+"");
+                        File tempFile = new File(imageFilePath);
+                        albumUri = Uri.fromFile(tempFile);
+                        cropImage();
+
+                    }else {
+                        Log.d("갤러리이미지로딩", "NULL");
+                    }
+                    break;
+                }
+                case CROP_PICTURE:{
+                    if(resultCode == Activity.RESULT_OK){
+                        Log.d("크롭", "OK");
+                        galleryAddPic();
+                        Log.d("크롭 갤러리 추가", "OK");
                         File tempFile = new File(imageFilePath);
                         BitmapFactory.Options options = new BitmapFactory.Options();
                         Bitmap bitmap = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
                         Log.d("갤러리 절대경로", tempFile.getAbsolutePath()+"");
 
                         if(bitmap != null){
-                            ExifInterface ei = new ExifInterface(tempFile.getAbsolutePath());
-                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
-                            int exifDegree = exifOrientationToDegrees(orientation);
-
-                            //Bitmap rotatedBitmap = rotateImage(bitmap, exifDegree);
                             setRecyclerViewData(imageFilePath);  // RecyclerView에 데이터 추가
-                            Log.d("갤러리 이미지 URI_re", ImageUri+"");
+                            Log.d("갤러리 이미지 URI_re", imageUri+"");
                         }
-                    }else {
-                        Log.d("갤러리이미지로딩", "NULL");
+
                     }
+                    break;
                 }
             }
 
---
+++ app/src/main/java/com/onePtwoL/acneanalyzer/ResultOfDiagnosis.java
@@ -0,0 +1,14 @@
+package com.onePtwoL.acneanalyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultOfDiagnosis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_of_diagnosis);
+    }
+}
