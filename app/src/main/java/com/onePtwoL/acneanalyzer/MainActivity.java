bc365b60e5fd222a29cabcd4b7e567743716582c
jun991203 <jun991203@naver.com>
2020-08-05 05:03:34 +0900

    덮어쓰기 방지 완료!

--- app/src/main/java/com/onePtwoL/acneanalyzer/MainActivity.java
+++ app/src/main/java/com/onePtwoL/acneanalyzer/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
     private static final int PICK_FROM_CAMERA = 1;
     private static final int PICK_FROM_ALBUM = 2;
     private static final int CROP_PICTURE = 3;
-    private static final int PICK_FROM_FILE = 4;
 
     private static Uri imageUri, albumUri;
     private String imageFilePath, imageFileName;
@@ -173,26 +172,31 @@ public class MainActivity extends AppCompatActivity {
 
     // 카메라 intent 연결하는 함수
     private void takePhotoWithCamera(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-                Log.d("카메라 찍음", photoFile.toString()+"");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                    Log.d("카메라 찍음", photoFile.toString()+"");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.onePtwoL.acneanalyzer.fileprovider", photoFile);
-                imageUri = photoURI;
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
-
-                Log.d("넘어감?", "OK");
+                if (photoFile != null) {
+                    Log.d("프로바이더", getPackageName()+"");
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.onePtwoL.acneanalyzer.fileprovider", photoFile);
+                    //imageUri = photoURI;
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
+                }
+            } else{
+                Log.d("카메라 저장공간", "접근 불가");
             }
         }
+
     }
 
     /* 이미지 파일 생성하는 함수 */
@@ -203,13 +207,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d("이미지 파일 타임스탬프", imageFileName+"");
 
         // 이미지가 저장될 폴더 이름(outline) & 빈 폴더 생성
-        //File storageDir = Environment.getExternalStorageDirectory(); // 외부 저장소 (공용공간) -> /storage/emulated/0/
-        File storageDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
+        File storageDir = Environment.getExternalStorageDirectory(); // 외부 저장소 (공용공간) -> /storage/emulated/0/
+        //File storageDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
         if(storageDir.exists()){
             storageDir.mkdirs();
         }
-        File image = null;
-        image = new File(storageDir, imageFileName);
+        File image = new File(storageDir, imageFileName);
         imageFilePath = image.getAbsolutePath();
         Log.d("이미지 파일 절대경로", imageFilePath+"");
         return image;
@@ -219,8 +222,8 @@ public class MainActivity extends AppCompatActivity {
     private void saveImage(Bitmap finalBitmap){
         String imageFileForGalName =  imageFileName;
         Log.d("갤러리 저장될 때", imageFileForGalName+"");
-        //File myDir = new File(Environment.getExternalStorageDirectory().toString());
-        File myDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
+        File myDir = new File(Environment.getExternalStorageDirectory().toString());
+        //File myDir = new File(Environment.getExternalStorageDirectory()+"/Pictures", "AcneAnalyzer");
         myDir.mkdirs();
         File file = new File(myDir, imageFileForGalName);
         try{
@@ -269,8 +272,8 @@ public class MainActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.setDataAndType(imageUri, "image/*");
 
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
+        intent.putExtra("aspectX", 300);
+        intent.putExtra("aspectY", 300);
         intent.putExtra("scale", true);
         intent.putExtra("output", albumUri);
         startActivityForResult(intent, CROP_PICTURE);
@@ -314,7 +317,10 @@ public class MainActivity extends AppCompatActivity {
                         imageFilePath = getImagePathFromURI(imageUri);
                         Log.d("갤러리이미지path", imageFilePath+"");
                         File tempFile = new File(imageFilePath);
-                        albumUri = Uri.fromFile(tempFile);
+
+                        File cropFile = createImageFile();
+                        albumUri = Uri.fromFile(cropFile);
+                        Log.d("앨범Uri", albumUri+"");
                         cropImage();
 
                     }else {
@@ -324,9 +330,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 case CROP_PICTURE:{
                     if(resultCode == Activity.RESULT_OK){
-                        Log.d("크롭", "OK");
                         galleryAddPic();
-                        Log.d("크롭 갤러리 추가", "OK");
                         File tempFile = new File(imageFilePath);
                         BitmapFactory.Options options = new BitmapFactory.Options();
                         Bitmap bitmap = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
